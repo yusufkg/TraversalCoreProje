4c9d589ef9600d2dc3c96a9e9fc35f35b7a792bd
Yusuf Kenan GİRGİN <yusufkenangirgin@hotmail.com>
2023-09-06 16:12:09 +0300

    v3

---
+++ TraversalCoreProje/Controllers/DestinationController.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Entityframework;
+using EntityLayer;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetlist();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+            ViewBag.i = id;
+            var values = destinationManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination p)
+        {
+            return View();
+        }
+    }
+}
---
+++ TraversalCoreProje/ViewComponents/Comment/_Commentlist.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Entityframework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.ViewComponents.Comment
+{
+    public class _Commentlist:ViewComponent
+    {
+        CommentManager commentManager = new CommentManager(new EfCommenDal());
+
+        public IViewComponentResult Invoke(int id)
+        {
+            var values = commentManager.TGetDestinationById(id);
+            return View(values);
+        }
+    }
+}
