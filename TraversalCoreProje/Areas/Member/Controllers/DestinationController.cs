b43eaf9e3e029d1bf26b5c737e88b50d1d4e4073
Yusuf Kenan GİRGİN <yusufkenangirgin@hotmail.com>
2023-09-11 14:09:01 +0300

    v6

---
+++ TraversalCoreProje/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Entityframework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Areas.Member.Controllers
+{
+    [AllowAnonymous]
+    [Area("Member")]
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+
+        public IActionResult Index()
+        {
+            var values= destinationManager.TGetlist();
+            return View(values);
+        }
+    }
+}
--- TraversalCoreProje/Controllers/LoginController.cs
+++ TraversalCoreProje/Controllers/LoginController.cs
@@ -14,10 +14,12 @@ namespace TraversalCoreProje.Controllers
     public class LoginController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public LoginController(UserManager<AppUser> userManager)
+        public LoginController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         [HttpGet]
@@ -58,6 +60,23 @@ namespace TraversalCoreProje.Controllers
         [HttpGet]
         public IActionResult SignIn()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignIn(UserSignInViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(p.username, p.passsword, false, true); 
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index","Profile", new { area="Member"});
+                }
+                else
+                {
+                    return RedirectToAction("SignIn", "Login");
+                }
+            }
             return View();
         }
     }
