34670b2934e57f5303520f39a5232fc0a1ce7fec
Yusuf Kenan GİRGİN <yusufkenangirgin@hotmail.com>
2023-09-25 12:05:07 +0300

    v14

---
+++ TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,80 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TraversalCoreProje.Models;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class CityController : Controller
+    {
+        private readonly IDestinationService _destinationService;
+
+
+        public CityController(IDestinationService destinationService)
+        {
+            _destinationService = destinationService;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+
+        public IActionResult CityList()
+        {
+            var jsonCity = JsonConvert.SerializeObject(_destinationService.TGetList());
+            return Json(jsonCity);
+        }
+
+        [HttpPost]
+        public IActionResult AddCityDestination(Destination destination)
+        {
+            destination.Status = true;
+            _destinationService.TAdd(destination);
+
+            var values = JsonConvert.SerializeObject(destination);
+           return Json(values);
+        }
+
+
+        public IActionResult GetById(int DestinationID)
+        {
+            var values = _destinationService.TGetByID(DestinationID);
+            var jsonValues = JsonConvert.SerializeObject(values);
+            return Json(jsonValues);
+        }
+
+        //public static List<CityClass> cities = new List<CityClass>
+        //{
+        //    new CityClass
+        //    {
+        //        CityID=1,
+        //        CityName="Üsküp",
+        //        CityCountry="Makedonya",
+        //    },
+        //    new CityClass
+        //    {
+        //        CityID=2,
+        //        CityName="Roma",
+        //        CityCountry="İtalya",
+        //    },
+        //    new CityClass
+        //    {
+        //        CityID=3,
+        //        CityName="Londra",
+        //        CityCountry="İngiltere"
+        //    }
+        //};
+
+    }
+}
