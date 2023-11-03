2cbe6ff73b0cccbf2589690298914cba6e9362fe
Yusuf Kenan GİRGİN <yusufkenangirgin@hotmail.com>
2023-11-03 14:20:56 +0300

    v25

--- TraversalCoreProje/Controllers/DefaultController.cs
+++ TraversalCoreProje/Controllers/DefaultController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProje.Controllers
 {
+
+    [AllowAnonymous]
     public class DefaultController : Controller
     {
         public IActionResult Index()
