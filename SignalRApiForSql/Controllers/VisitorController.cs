ddd249bd2ae12e82a74e4222c8fa9e4291b5065a
Yusuf Kenan GİRGİN <yusufkenangirgin@hotmail.com>
2023-11-28 16:15:47 +0300

    v34

---
+++ SignalRApiForSql/Controllers/VisitorController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SignalRApiForSql.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRApiForSql.Controllers
+{
+    public class VisitorController : Controller
+    {
+        public IActionResult Index()
+        { private readonly VisitorService _visitorService;
+
+        public VisitorController(VisitorService visitorService)
+        {
+            _visitorService = visitorService;
+        }
+        [HttpGet]
+        public IActionResult CreateVisitor()
+        {
+            Random random = new Random();
+            Enumerable.Range(1, 10).ToList().ForEach(x =>
+            {
+                foreach (ECity item in Enum.GetValues(typeof(ECity)))
+                {
+                    var newVisitor = new Visitor
+                    {
+                        City = item,
+                        CityVisitCount = random.Next(100, 2000),
+                        VisitDate = DateTime.Now.AddDays(x)
+                    };
+                    _visitorService.SaveVisitor(newVisitor).Wait();
+                    System.Threading.Thread.Sleep(1000);
+                }
+            });
+            return Ok("Ziyaretçiler başarılı bir şekilde eklendi");
+        }
+        }
+    }
+}
