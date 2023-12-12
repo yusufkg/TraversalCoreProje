7cb8a9c1680a606cbb4f363a44b5557469bb94e0
Yusuf Kenan GİRGİN <yusufkenangirgin@hotmail.com>
2023-12-12 10:02:54 +0300

    v41

--- TraversalCoreProje/Areas/Member/Controllers/DashboardController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/DashboardController.cs
@@ -1,12 +1,15 @@
 ﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace TraversalCoreProje.Areas.Member.Controllers
 {
     [Area("Member")]
-
     public class DashboardController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
@@ -15,12 +18,16 @@ namespace TraversalCoreProje.Areas.Member.Controllers
         {
             _userManager = userManager;
         }
-
-        public async Task<IActionResult>  Index()
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
         {
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
             ViewBag.userName = values.Name + " " + values.Surname;
             ViewBag.userImage = values.ImageUrl;
+            return View();
+        }
+        public async Task<IActionResult> MemberDashboard()
+        {
             return View();
         }
     }
