8d1d696729877dbc4a95f4810870e54bd6baf52a
Yusuf Kenan GİRGİN <yusufkenangirgin@hotmail.com>
2023-12-19 15:58:15 +0300

    v45

--- TraversalCoreProje/Startup.cs
+++ TraversalCoreProje/Startup.cs
@@ -1,34 +1,21 @@
-using BusinessLayer.Abstract;
-using BusinessLayer.Concrete;
-using DataAccesslayer.Abstract;
+using BusinessLayer.Container;
 using DataAccesslayer.Concrete;
-using DataAccesslayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.AspNetCore;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Primitives;
-using BusinessLayer.Container;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using TraversalCoreProje.Models;
-using Extensions = BusinessLayer.Container.Extensions;
 using Microsoft.Extensions.Logging;
 using System.IO;
-using FluentValidation;
-using DTOLayer.DTOs.AnnouncementDTOs;
-using BusinessLayer.ValidationRules;
-using FluentValidation.AspNetCore;
 using TraversalCoreProje.CQRS.Handlers.DestinationHandlers;
-using MediatR;
-using Microsoft.AspNetCore.Mvc.Razor;
+using TraversalCoreProje.Models;
 
 namespace TraversalCoreProje
 {
@@ -52,7 +39,6 @@ namespace TraversalCoreProje
 
             services.AddMediatR(typeof(Startup));
 
-
             services.AddLogging(x =>
             {
                 x.ClearProviders();
@@ -60,9 +46,8 @@ namespace TraversalCoreProje
                 x.AddDebug();
             });
 
-
             services.AddDbContext<Context>();
-            services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>().AddErrorDescriber<CustomIdentityValidator>().AddEntityFrameworkStores<Context>();
+            services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>().AddErrorDescriber<CustomIdentityValidator>().AddTokenProvider<DataProtectorTokenProvider<AppUser>>(TokenOptions.DefaultProvider).AddEntityFrameworkStores<Context>();
 
             services.AddHttpClient();
 
@@ -72,17 +57,16 @@ namespace TraversalCoreProje
 
             services.CustomerValidator();
 
-             services.AddControllersWithViews().AddFluentValidation();
-
+            services.AddControllersWithViews().AddFluentValidation();
 
 
 
             services.AddMvc(config =>
             {
                 var policy = new AuthorizationPolicyBuilder()
-                    .RequireAuthenticatedUser()
-                    .Build();
-                    config.Filters.Add(new AuthorizeFilter(policy));
+                .RequireAuthenticatedUser()
+                .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
             });
 
             services.AddLocalization(opt =>
@@ -91,7 +75,6 @@ namespace TraversalCoreProje
             });
 
             services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix).AddDataAnnotationsLocalization();
-
 
             services.ConfigureApplicationCookie(options =>
             {
@@ -105,7 +88,6 @@ namespace TraversalCoreProje
             var path = Directory.GetCurrentDirectory();
             loggerFactory.AddFile($"{path}\\Logs\\Log1.txt");
 
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -118,25 +100,23 @@ namespace TraversalCoreProje
             }
 
             app.UseStatusCodePagesWithReExecute("/ErrorPage/Error404", "?code={0}");
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
-            app.UseRouting();
             app.UseAuthentication();
-            app.UseAuthorization();
+            app.UseRouting();
 
+            app.UseAuthorization();
 
             var suppertedCultures = new[] { "en", "fr", "es", "gr", "tr", "de" };
             var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(suppertedCultures[1]).AddSupportedCultures(suppertedCultures).AddSupportedUICultures(suppertedCultures);
-
             app.UseRequestLocalization(localizationOptions);
-
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Default}/{action=Index}/{id?}");
             });
 
             app.UseEndpoints(endpoints =>
@@ -146,7 +126,7 @@ namespace TraversalCoreProje
                   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
                 );
             });
-     
+
         }
     }
 }
